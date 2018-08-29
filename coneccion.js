be6b78b54b1978bf1a4ed187d9c0340b4767706e
darwinj94 <darwinj94@hotmail.com>
2018-08-29 12:51:20 -0500

    crud ruta completa

--- coneccion.js
+++ coneccion.js
@@ -4,13 +4,12 @@ var options = {
   // Initialization Options
   promiseLib: promise
 };
-
 var pgp = require('pg-promise')(options);
 var connectionString = "postgres://postgres:darwin123@localhost:5432/inventariodb";
 var db = pgp(connectionString);
 
 // add query functions
-function getAllPuppies(req, res, next) {
+function getTipos(req, res, next) {
     db.any('select * from tipo')
       .then(function (data) {
         res.status(200)
@@ -25,7 +24,69 @@ function getAllPuppies(req, res, next) {
       });
   }
 
-  function createPuppy(req, res, next) {
+  //se recupera el total de tipos para la paginacion
+  // select count(*)
+  // from libros
+  // where editorial='Planeta'
+  function getTotalTipos(req, res, next) {
+    db.any("select count(*)  from tipo where estado=1")
+      .then(function (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'Se obtuvo el total de registros de la tabla tipo'
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+
+
+  function getTipo(req, res, next){
+    console.log(req);
+    let id=req.params.id;
+    db.any('select * from tipo where idtipo='+id)
+      .then(function (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'se optuvo el material'
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+  
+  function getTiposPaginacion(req, res, next) {
+    console.log(req.body);
+    var page=req.body.page;
+    var itemsPerPage=req.body.itemsPerPage;
+    console.log(page);
+    console.log(itemsPerPage);
+    //db.any('SELECT * FROM tipo LIMIT {'+itemsPerPage+'} OFFSET {('+page - 1+') * '+itemsPerPage+'}')
+    // [LIMIT { number | ALL }] [OFFSET number]
+    // db.any('SELECT * FROM tipo LIMIT 7 OFFSET 7')
+
+    page2=page*itemsPerPage;
+    db.any('SELECT * FROM tipo LIMIT '+itemsPerPage+' OFFSET '+page2)
+      .then(function (data) {
+        res.status(200)
+          .json({
+            status: 'success',
+            data: data,
+            message: 'Retrieved ALL tipos'
+          });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+
+  function crudTipo(req, res, next) {
     console.log([req.body.idtipo,req.body.nombre,req.body.estado,req.body.fecha]);
     var SQL = 'select * from  fun_ime_tipo($1, $2, $3);';
     db.any(SQL, [req.body.idtipo,req.body.nombre,req.body.opcion])
@@ -41,9 +102,12 @@ function getAllPuppies(req, res, next) {
 
 
 module.exports = {
-  getAllPuppies: getAllPuppies,
+  getTipos: getTipos,
+  getTipo:getTipo,
 // getSinglePuppy: getSinglePuppy,
-   createPuppy: createPuppy,
+  crudTipo: crudTipo,
+  getTiposPaginacion: getTiposPaginacion,
+  getTotalTipos:getTotalTipos
 // updatePuppy: updatePuppy,
 // removePuppy: removePuppy
 };
