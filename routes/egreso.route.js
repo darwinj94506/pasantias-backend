45093a4094a5c1763262ac76e578ee0fea90aed5
darwinj94 <darwinj94@hotmail.com>
2018-09-21 12:01:14 -0500

    voy a pone rpermisos en rutas

--- routes/egreso.route.js
+++ routes/egreso.route.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var EgresoController=require('./../controllers/egreso.controller');
+var md_auth=require('../middlewares/authenticated');
 
 router.post('/crudEgreso',EgresoController.crudEgreso);
 router.post('/crudDetalle',EgresoController.crudDetalle);
 router.post('/crudDetalle2',EgresoController.crudDetalle2);
-
 router.post('/getEgresosPaginacion',EgresoController.getEgresosPaginacion);
 router.post('/getTotalEgresos',EgresoController.getTotalEgresos);
 router.post('/validarDetalle',EgresoController.validarDetalle);
